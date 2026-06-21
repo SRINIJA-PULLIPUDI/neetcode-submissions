400d1ad41b04b165b613116f489c90e66ef81e5f
PULLIPUDI SRINIJA <23MH1A05O3@acoe.edu.in>
2026-06-21 23:25:09 +0530

    Add: sort-colors - submission-5

---
+++ Data Structures & Algorithms/sort-colors/submission-5.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i=0,k=nums.size()-1;
+        for(int j=0;j<nums.size();j++){
+            if(j>k) break;
+            if(nums[j]!=1) {
+                if(nums[j]==0){
+                    swap(nums[i],nums[j]);
+                    i++;
+                }else{
+                    swap(nums[j],nums[k]);
+                    k--;
+                    j--;
+                }
+            }
+
+            
+        }
+    }
+};
