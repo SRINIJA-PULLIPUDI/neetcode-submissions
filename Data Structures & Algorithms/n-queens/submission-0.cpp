b156fb191823481e11abe5a07f674c1fe93876a3
PULLIPUDI SRINIJA <23MH1A05O3@acoe.edu.in>
2026-07-15 13:59:03 +0530

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+vector<vector<string>> res;
+    bool check(vector<vector<int>>& queen, int n, int row, int col){
+        for(int i=0;i<n;i++){
+            if(queen[row][i]) return false;
+            if(queen[i][col]) return false;
+        }
+        if(col>=row){
+            int k=0;
+            for(int i=(col-row);i<n;i++){
+                if(queen[k++][i]) return false;
+            }
+        }else{
+            int k=0;
+            for(int i= (row-col);i<n;i++){
+                if(queen[i][k++]) return false;
+            }
+        }
+        if((row+col)<= (n-1)){
+            int k = 0;
+            for(int i=row+col;i>=0;i--){
+                if(queen[k++][i]) return false;
+            }
+        }else{
+            int k = n-1;
+            for(int i=(row+col-n+1);i<n;i++){
+                if(queen[i][k--]) return false;
+            }
+        }
+        return true;
+    }
+    void solve(vector<vector<int>> queen, int n, int col, int row, int idx){
+        if(idx==col) {
+            solve(queen, n, col+1, row, idx);
+        }
+        if(col == n){
+            vector<string> k;
+            for(int i=0;i<n;i++){
+                string s = "";
+                for(int j=0;j<n;j++){
+                    if(queen[i][j]) s+='Q';
+                    else s+='.';
+                }
+                k.push_back(s);
+            }
+            res.push_back(k);
+            return;
+        }
+        for(int i=0;i<n;i++){
+            if(check(queen, n, i, col)){
+                queen[i][col] = 1;
+                solve(queen, n, col+1, 0, idx);
+                queen[i][col] = 0;
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<int>> queen(n,vector<int> (n));
+        for(int i=0;i<n;i++){
+            queen[0][i] = 1;
+            solve(queen,n,0,0,i);
+            queen[0][i] = 0;
+        }
+        return res;
+    }
+};
