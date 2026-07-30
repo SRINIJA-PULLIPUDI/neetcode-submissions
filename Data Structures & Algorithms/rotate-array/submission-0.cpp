b51731605da45d3d60ccd7a6d0f4642150132f55
PULLIPUDI SRINIJA <23MH1A05O3@acoe.edu.in>
2026-07-30 10:10:56 +0530

    Add: rotate-array - submission-0

---
+++ Data Structures & Algorithms/rotate-array/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k%=nums.size();
+        int i= 0,j=(nums.size()-k-1);
+        while(i<j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+        }
+        i=(nums.size()-k),j = nums.size()-1;
+        while(i<j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+        }
+        reverse(nums.begin(),nums.end());
+    }
+};
